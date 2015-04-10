871073f522293f9725fbdc6f685354567f67d981
Vipul Kashyap <vipulkashyap@ucla.edu>
2015-04-09 20:38:01 -0700

    Fixed image panel enlargement issue

--- js/content.js
+++ js/content.js
@@ -10,7 +10,7 @@ chrome.storage.sync.get(
 		voteBombEnabled: true,
 		largeImageModeEnabled: false,
 		sideGalleryRemoveEnabled: false,
-	}, 
+	},
 	function (items)
 	{
 		if(items.youTagEnabled)
@@ -18,10 +18,10 @@ chrome.storage.sync.get(
 
 		if(items.loadingGIFReplacementEnabled)
 			startLoadingGifResizing(items.loaderScaleFactor);
-		
+
 		if(items.oldBarEnabled)
 			startOldBarInject();
-		
+
 		if(items.voteBombEnabled)
 			detectVoteBombs();
 
@@ -30,7 +30,7 @@ chrome.storage.sync.get(
 
 		if(items.sideGalleryRemoveEnabled)
 			removeSideGallery(items.largeImageModeEnabled);
-		
+
 		/*
 		var dataString = $('script[type="text/javascript"]:contains("widgetFactory.mergeConfig"):eq(2)').text();
 		dataString = dataString.substring(dataString.indexOf("image"), dataString.indexOf("group"));
@@ -38,7 +38,7 @@ chrome.storage.sync.get(
 		var imageData = JSON.parse(dataString);
 		image data extraction... may be useful later
 		*/
-		
+
 	});
 
 function startYouTagWatcher()
@@ -47,7 +47,7 @@ function startYouTagWatcher()
 	$('#comments').bind('DOMNodeInserted DOMNodeRemoved', function() {
 		// Find out the user's username
 		var usersName = $(".account-user-name").text();
-		
+
 		// If there is a comment with the same username, add a YOU tag
 		var commentAuthors = $(".author a")
 		.filter(function(index)
@@ -64,25 +64,25 @@ function startYouTagWatcher()
 }
 
 function startLoadingGifResizing(scaleFactor)
-{	
+{
 	// Run each time the body is modified
 	$('body').bind('DOMNodeInserted DOMNodeRemoved', function() {
 		// If no loading gif is present, do nothing
 		if($(".tipsy-inner img").length == 0)
 			return;
-		
+
 		// Otherwise calculate a new size
 		var newSize = $(".tipsy-inner img").width();
-		
+
 		if(newSize <= 30)
 			newSize *= scaleFactor;
-		
+
 		// Update the size
 		$(".tipsy-inner img").css("width", newSize + "px");
 	});
 
 	// Inject CSS for resizing other images
-	$('head').append('<style id="d"> #side-gallery .small-loader, #small-loader {background-repeat: no-repeat !important; background-size:' + scaleFactor*40 + 'px auto !important; } #side-gallery .small-loader, #small-loader { height: ' + scaleFactor*40 + 'px !important; width: ' + scaleFactor*40 + 'px  !important; }  #cboxLoadingGraphic, .zoom-loader { height: auto !important;  width: ' + scaleFactor*40 + 'px !important;}  .outside-loader { width: ' + scaleFactor*40 + 'px !important; height: auto !important; }  #past-wrapper #past-loader { width:' + scaleFactor*48 + 'px !important; height:auto !important; }  #shareonimgur #share-loader { width:' + scaleFactor*24 + 'px !important; height:auto !important; } </style>'); 
+	$('head').append('<style id="d"> #side-gallery .small-loader, #small-loader {background-repeat: no-repeat !important; background-size:' + scaleFactor*40 + 'px auto !important; } #side-gallery .small-loader, #small-loader { height: ' + scaleFactor*40 + 'px !important; width: ' + scaleFactor*40 + 'px  !important; }  #cboxLoadingGraphic, .zoom-loader { height: auto !important;  width: ' + scaleFactor*40 + 'px !important;}  .outside-loader { width: ' + scaleFactor*40 + 'px !important; height: auto !important; }  #past-wrapper #past-loader { width:' + scaleFactor*48 + 'px !important; height:auto !important; }  #shareonimgur #share-loader { width:' + scaleFactor*24 + 'px !important; height:auto !important; } </style>');
 }
 
 function startOldBarInject()
@@ -90,20 +90,20 @@ function startOldBarInject()
 	// Each time the image is changed, tell imgur to update the stats
 	$('.under-title-info').bind('DOMNodeInserted DOMNodeRemoved', function() {
 		// Open the stats box invisibly for data extraction
-		$(".stats-link").click(); $(".analyticsOnly").hide(); $("#cboxClose").click(); $(".analyticsOnly").show(); 
+		$(".stats-link").click(); $(".analyticsOnly").hide(); $("#cboxClose").click(); $(".analyticsOnly").show();
 	});
-	
+
 	// Each time the analytics are updated, update the infobar
 	$('#info-analytics').bind('DOMNodeInserted DOMNodeRemoved', function() {
 		// Merge the two stat lines into one
 		$(".stats-link br").remove();
 		$(".point-info").css("margin-right", "12px");
 		$(".stats-link").css("width", "30%");
-		
+
 		// Get upvote/downvote counts
 		var upvoteCount = parseInt($(".ups-total .value").text().replace(/,/g, ''));
 		var downvoteCount = parseInt($(".downs-total .value").text().replace(/,/g, ''));
-		
+
 		// Inject stat bar
 		var oldStats = '<div id="oldStatBar" style="height: 0.6em; width: 78%; max-width: 300px; margin-top:1.7em; background-color:#e44;"> <div style="width: ' + upvoteCount*100/(upvoteCount+downvoteCount) + '%; height: 100%; background-color: #85BF25;"> </div>';
 		if($("#oldStatBar").length == 0)
@@ -130,11 +130,11 @@ function voteBomb(upOdown)
 		vbType = 'na';
 		return;
 	}
-	
+
 	// Otherwise upvote everything
 	$(upOdown).not('.pushed').click();
 	vbType = upOdown;
-	
+
 	// Also press the main arrow button if necessary
 	var mainArrow;
 	if(upOdown == '.up')
@@ -178,5 +178,9 @@ function removeSideGallery(largePanelEnabled)
 	$(".advertisement").remove();
 	$("#content").css("width", "610px");
 	if(largePanelEnabled)
-		$("#content").css("width", "63em");
+	{
+		$("#content").css("width", "60em");
+		$(".main-image .panel").css("width", "60em");
+		$("#comments-container").css("width", "60em");
+	}
 }
